ff987e73eeb4b7a593a1ac2ccbf77fb2ad9a58cc
mfventures01-maker <mfventures01@gmail.com>
2026-06-06 22:50:26 +0100

    fix: mount AuthProvider at application root

--- index.tsx
+++ index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './src/auth/AuthProvider';
 import './index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+<React.StrictMode> <AuthProvider> <BrowserRouter> <App /> </BrowserRouter> </AuthProvider>
+</React.StrictMode>
 );
